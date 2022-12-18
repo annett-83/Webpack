dfd5dfdda7b7ec5934cd28b7dababb95eedde91c
nova8 <aaa.auksutat@gmail.com>
2022-12-18 21:39:10 +0100

    webpack full

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "../index.css";
+import JS_IMAGE from "../assets/javascript.png";
+
+console.log('Helo World');
+
+const jsImageHtml = document.createElement("img");
+jsImageHtml.src = JS_IMAGE;
+
+document.body.append(jsImageHtml);
